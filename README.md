0dbfcbf528192f88b73a8231c19ecfd84d4b0680
haluk <hdogan@vivaldi.net>
2019-09-15 19:02:46 -0500

    python showcase src and test

---
+++ README.md
@@ -0,0 +1,8 @@
+# base16-dunst
+
+This repository is meant to work with
+[chriskempson/base16](https://github.com/chriskempson/base16).
+It provides a simple template that can be used with the base16 color schemes to
+generate a functional config file for
+[knopwob/dunst](https://github.com/knopwob/dunst),
+a lightweight and customizable notification daemon.
---
+++ test/test_util.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import configparser
+import os
+import unittest
+
+from src.util import get_sections_only, append_section
+
+
+class TestConfig(unittest.TestCase):
+    def setUp(self):
+        self.fname = "themes/base16-3024.dunstrc"
+        self.outconfig = "/tmp/example.ini"
+        self.config = configparser.ConfigParser()
+
+    def test_can_get_sections(self):
+        _, self.config = get_sections_only(self.fname)
+
+        self.assertEqual(3, len(self.config.sections()))
+        self.assertListEqual(
+            ["base16_low", "base16_normal", "base16_critical"], self.config.sections()
+        )
+
+    def test_can_append_section(self):
+        self.config = append_section(self.fname)
+
+        self.assertEqual(4, len(self.config.sections()))
+        self.assertListEqual(
+            ["base16_low", "base16_normal", "base16_critical", "general"],
+            self.config.sections(),
+        )
+
+    def test_can_write_config(self):
+        self.config = append_section(self.fname)
+
+        with open(self.outconfig, "w") as configfile:
+            self.config.write(configfile)
+
+        self.assertTrue(os.path.isfile(self.outconfig))
+
+        # os.remove(self.outconfig)
